9d265ab44c68e27b84ea97dde485945e8a7e9c59
Jenifa Rawshan <jenifarawshan057@gmail.com>
2022-09-01 20:51:40 +0600

    feat: add skeleton sidebar

---
+++ src/components/SideBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <b-sidebar v-model="isOpen" right :title="title" width="576px" />
+  </div>
+</template>
+
+<script>
+import store from '@/store';
+import config from '@/../configs/config.json';
+
+export default {
+  name: 'SideBar',
+  computed: {
+    isOpen: {
+      get() {
+        return store.getters.isSideBarOpen;
+      },
+      set(open) {
+        store.commit('setSideBarOpen', open);
+      },
+    },
+  },
+  data: () => ({
+    title: config.flow.carousel.defaultTitle
+  }),
+};
+</script>
+
+<style scoped>
+::v-deep .b-sidebar {
+  background-color: white !important;
+}
+
+::v-deep .b-sidebar .b-sidebar-header {
+  display: flex;
+  padding: 0;
+}
+</style>
